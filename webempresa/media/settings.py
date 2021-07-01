caf6bc417b48315785da9496aae6c79db531bac6
Tzintzunjhonna <jjctjhonathan@gmail.com>
2021-07-01 11:24:15 -0500

    Creando vista de blog

---
+++ webempresa/media/settings.py
@@ -0,0 +1,3 @@
+# Media config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'services.apps.ServicesConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
